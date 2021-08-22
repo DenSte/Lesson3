399d3a814f032b8130d19cccd1bd6289dc39c8b8
Denys <stepasiukdenys@gmail.com>
2021-08-21 23:27:53 -0400

    Lesson3 train_station.rb second attempt

--- train_station.rb
+++ train_station.rb
@@ -1,70 +1,68 @@
+# frozen_string_literal: true
+
 class Station
+  attr_reader :name, :trains
 
-  attr_reader :station_name, :trains
-
-  def initialize(station_name)
-
-    @station_name = station_name
-    puts "Station Name: #{station_name}"
+  def initialize(name)
+    @name = name
+    puts "Station Name: #{name}"
     @trains = []
   end
 
   def get_train(train)
     trains << train
-    puts "To the #{station_name} station arrived train #{train.number}"
+    puts "To the #{name} station arrived train #{train.number}"
   end
 
-   def show_trains(type = nil)
-      if type
-        puts "Trains at the #{station_name} type #{type}: "  
-        trains.each{|train| puts train.number if train.type == type}
-      else
-        puts "Trains at the #{station_name} station"
-        trains.each{|train| puts train.number}
-      end
+  def strains_by_type(type)
+    puts "Trains at the #{name} type #{type}: "
+    trains.each { |train| puts train.number if train.type == type }
+  end
 
   def send_train(train)
     trains.delete(train)
     train.station = nil
-    puts "From the station #{station_name} departure train № #{train.number}"
-  end
+    puts "From the station #{name} departure train № #{train.number}"
   end
 end
 
-
 class Route
+  attr_accessor :stations
 
-  attr_accessor :stations, :from, :to
-
-  def initialize (from, to)
+  def initialize(from, to)
     @stations = [from, to]
-    puts "Route from  #{from.station_name} to #{to.station_name}"
+    puts "Route from #{from.name} to #{to.name}"
+  end
+
+  def first_station
+    station.first
+  end
+
+  def last_station
+    station.last
   end
 
   def add_station(station)
-    self.stations.insert(-2, station) 
-    puts "Route from #{stations.first.station_name} - to #{stations.last.station_name} station #{station.station_name} was added"
+    stations.insert(-2, station)
+    puts "Route from #{first_station.name} - to #{last_station.name} station #{station.name} was added"
   end
 
   def remove_station(station)
-    if [stations.first, stations.last].include?(station)
+    if [first_station, last_station].include?(station)
       puts "Departure and Arrival station of the route can't be deleted"
-    else 
-      self.stations.delete(station)
-      puts "From the route #{stations.first.station_name} - #{stations.last.station_name} station #{station.station_name} has been deleted "
+    else
+      stations.delete(station)
+      puts "From the route #{first_station.name} - #{last_station.name} station #{station.name} has been deleted "
     end
   end
 
   def display_stations
-    puts "Route  #{stations.first.station_name} - #{stations.last.station_name} includes: "
-    stations.each{|station| puts " #{station.station_name}" }
-  end  
+    puts "Route #{first_station.name} - #{last_station.name} includes: "
+    stations.each { |station| puts " #{station.name}" }
+  end
 end
 
-
-
 class Train
-
   attr_accessor :speed, :number, :number_of_carriages, :route, :station
   attr_reader :type
 
@@ -73,71 +71,62 @@ class Train
     @type = type
     @number_of_carriages = number_of_carriages
     @speed = 0
+    @current_station_index = 0
+
     puts "Train № #{number} is formed. Train type: #{type}. Number of carriages: #{number_of_carriages}."
   end
 
-  def stop 
+  def stop
     self.speed = 0
   end
 
- def acceleration(speed)
+  def accelerate(speed)
     self.speed = speed if speed > self.speed
   end
 
-  def braking
+  def brake
     self.speed = 0
   end
 
-
-  
-
   def add_carriages
-    if speed.zero? 
+    if speed.zero?
       self.number_of_carriagest += 1
       puts "To the train №#{number} carriage was added. Now number of carrieages is #{number_of_carriages}."
-    else 
-      puts "While thetrain is moving carriages can't be added"
+    else
+      puts "While the train is moving carriages can't be added"
     end
   end
 
   def remove_carriages
     if number_of_carriages.zero?
-      puts "This train do not have any carriages"
-    elsif speed.zero? 
+      puts 'This train do not have any carriages'
+      elseif speed.zero?
       self.number_of_carriages -= 1
       puts "From the train №#{number} carriage was removed. Now number of carrieages is #{number_of_carriages}."
-    else 
-      puts "While thetrain is moving carriages can't be removed"
+    else
+      puts "While the train is moving carriages can't be removed"
     end
   end
 
-  def destination(route)
+  def set_rout(route)
     self.route = route
-    puts "Train №#{number} taking rout #{route.stations.first.station_name} - #{route.stations.last.station_name}" 
+    @current_station_index = 0
+    puts "Train №#{number} taking rout #{route.first_station.name} - #{route.last_station.name}"
   end
 
-  def next_station(station)
-    if route.nil?
-      puts "Train can't be without destination'"
-    elsif @station == station
-      puts "Train №#{@number} already at the #{@station.station_name}"
-    elsif route.stations.include?(station)
-      @station.send_train(self) if @station
-      @station = station
-      station.get_train(self)
-    else
-      puts "Train №#{number} is not going through station #{station.station_name}  "
+  def go_next_station
+    @current_station_index += 1 if @current_station_index != @current_station_index.last && !@current_station_index.nil?
+  end
+
+  def go_previous_station
+    if @current_station_index.positive? != @current_station_index.first && !@current_station_index.nil?
+      @current_station_index -= 1
     end
   end
 
-  def current_stations
-    if route.nil?
-      puts "Route is not set"
-    else
-      station_index = route.stations.index(station)
-      puts "Train № #{number} currently at #{station.station_name}."
-      puts "Previous station - #{route.stations[station_index - 1].station_name}." if station_index != 0
-      puts "Next station - #{route.stations[station_index + 1].station_name}." if station_index != route.stations.size - 1
-    end
+  def nearest_stations_list(route)
+    stations = route.stations
+    { previous_station: stations[@current_station_index -= 1], current_station_index: [@current_station_index],
+      next_station_index: [@current_station_index += 1] }
   end
 end
